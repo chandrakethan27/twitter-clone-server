d7c2f0ef2a54837357470f3f221589e2c6c98778
Chandrakethan <90309231+chandrakethan27@users.noreply.github.com>
2024-06-02 19:58:12 +0530

    Delete build directory

--- build/app/tweet/queries.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queries = void 0;
-exports.queries = `#graphql
-    getAllTweets: [Tweet]
-    getSignedURLForTweet( imageName: String!, imageType: String!) :String
-`;
